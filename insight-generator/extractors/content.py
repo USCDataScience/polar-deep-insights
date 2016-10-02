b02ec8cea116b3069e6592054ab100533553ff46
Nithin Krishna <nithinkrishh@gmail.com>
2016-10-01 20:32:43 -0700

    quick fix speech recognizer

--- insight-generator/extractors/content.py
+++ insight-generator/extractors/content.py
@@ -1,6 +1,10 @@
 # Required modules
 # tika-parser: from tika import parser
 
+import shlex
+from subprocess import call
+import speech_recognition as sr
+
 class ContentExtractor:
   def __init__(self, path, modules):
     self.__modules = modules
@@ -13,8 +17,10 @@ class ContentExtractor:
     self.content_type = parsed["metadata"]["Content-Type"]
 
     if self.isExtractableImage():
-      d = self.imageObjectExtractor()
-      content = "\n".join(d)
+      content = self.imageObjectExtractor() + content
+
+    if self.isExtractableAudio():
+      content = self.audioSpeechExtractor() + content
 
     if include_metadata:
       md_string = "\n".join( map(str, parsed["metadata"].values()) )
@@ -25,5 +31,36 @@ class ContentExtractor:
   def isExtractableImage(self):
     return self.content_type == "image/jpeg"
 
+  def isExtractableAudio(self):
+    return self.content_type[0:5] == "audio"
+
+  def audioSpeechExtractor(self):
+    try:
+      tmp = self.__modules["TmpFile"](extension=".flac")
+      # Convert file to .flac
+      call(shlex.split("ffmpeg -i {0} -c:a flac {1}".format(self.path, tmp.path)))
+      r = sr.Recognizer()
+      with sr.AudioFile(tmp.path) as source:
+        audio = r.record(source)
+      HOUNDIFY_CLIENT_ID = "r0m3MxqkxG_r5fKQOt6qaw=="
+      HOUNDIFY_CLIENT_KEY = "u2s0qH3kHm8jAntPSf46ISe87JUvUwaAi03iJYcsXTyiXISDom825P1sEVW-6gU8q3td8Coe5dWFQXN3TGdf0Q=="
+      d = r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY)
+      # Delete tmp file
+      tmp.destroy()
+
+      print "EXTRACTED TEXT {0}".format(d)
+      return d
+    except Exception as e:
+      print "Speech conversion error {0}".format(e)
+      return ""
+
   def imageObjectExtractor(self):
-    return self.__modules["TikaWrapper"](self.path, self.__modules).getImageObjects()
+    try:
+      d = self.__modules["TikaWrapper"](self.path, self.__modules).getImageObjects()
+    except Exception as e:
+      print "Image object extraction error {0}".format(e)
+      d = [ ]
+
+    return "\n".join( d )
+
+

bb5f1918e5c3effcb7c56a7f9fd423ae97cd2cd6
Nithin Krishna <nithinkrishh@gmail.com>
2016-10-03 20:14:55 -0700

    env files

---
+++ insight-generator/env.sh
@@ -0,0 +1,4 @@
+export REDIS_ENDPOINT=52.8.18.97
+export TIKA_PATH=http://52.8.18.97:80
+export GROBID_QUANTITY_ENDPOINT=http://52.8.18.97/grobid-quantities
+export GEO_TOPIC_ENDPOINT=http://52.8.18.97/geo-topic

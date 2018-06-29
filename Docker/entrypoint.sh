33ebbea48aa753265655fbfdf4c66b4dde5b79a9
Preranathm <prenastro@users.noreply.github.com>
2018-06-28 20:13:52 -0700

    Update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # encoding: utf-8
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with

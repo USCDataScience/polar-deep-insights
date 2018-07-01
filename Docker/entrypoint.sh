c22f90af359c92547c44b90e349f02d6498be3cd
Preranathm <prenastro@users.noreply.github.com>
2018-06-30 18:23:51 -0700

    Update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -27,7 +27,7 @@ if [ "${cmd}" == "grunt serve" ]; then
     fi
 
     if [ -f 'bower.json' ]; then
-        bower cache clean && bower install || exit
+        bower install || exit
     else 
         echo "No 'bower.json' inside $(pwd). Skipping 'bower install'."
     fi

09e50925ec3967062e35f6b41f78c9485d2e7500
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2018-08-28 09:15:37 -0700

    Cleanup of PDI insights generator docker.

--- Docker/insight-generator/entrypoint.sh
+++ Docker/insight-generator/entrypoint.sh
@@ -19,29 +19,70 @@ SPARKLER_JSON_PATH=/data/polar
 
 mkdir -p /deploy/requirements/logs/
 touch /deploy/requirements/logs/pdi-generator.log
+touch /deploy/requirements/logs/pdi-ingest.log
 
-echo "Starting Apache2 (HTTPD)"
-sudo apachectl start
+WAIT_COMMAND='[ $(curl --write-out %{http_code} --silent --output /dev/null http://polar-deep-insights/elasticsearch/_cat/health?h=st) = 200 ]'
+WAIT_LOOPS=10
+WAIT_SLEEP=2
+
+is_ready() {
+    eval "$WAIT_COMMAND"
+}
+
+# wait until is ready
+i=0
+while ! is_ready; do
+    i=`expr $i + 1`
+    if [ $i -ge $WAIT_LOOPS ]; then
+        echo "$(date) - Elasticsearch still not ready, giving up"
+        exit 1
+    fi
+    echo "$(date) - waiting for Elasticsearch to be ready"
+    sleep $WAIT_SLEEP
+done
 
 echo "Starting Grobid Quantities"
 cd /deploy/requirements/grobid/grobid-quantities && \
-    mvn -Dmaven.test.skip=true jetty:run-war >> /deploy/requirements/pdi-generator.log 2>&1&
+    mvn -Dmaven.test.skip=true jetty:run-war >> /deploy/requirements/logs/pdi-generator.log 2>&1&
 
 echo "Starting Tika Server"
 cd /deploy/requirements/tika-server && \
-    java -jar tika-server-1.15-SNAPSHOT.jar >> /deploy/requirements/pdi-generator.log 2>&1&
+    java -jar tika-server-1.15-SNAPSHOT.jar >> /deploy/requirements/logs/pdi-generator.log 2>&1&
 
 echo "Starting Lucene Geo Gazetteer Server"
 cd /deploy/requirements/lucene-geo-gazetteer && \
-    lucene-geo-gazetteer -server >> /deploy/requirements/pdi-generator.log 2>&1&
+    lucene-geo-gazetteer -server >> /deploy/requirements/logs/pdi-generator.log 2>&1&
 
+if [[ ! -z "${PDI_JSON_PATH}" ]]; then
 
-if [ -d "$SPARKLER_JSON_PATH/sparkler.json" && -d "$ES_URL" ]; then
-    echo "Using SPARKLER JSON and loading into Elasticsearch: [$ES_URL]: JSON path: $SPARKLER_JSON_PATH/sparkler.json"
-    # add command here
+    if [ -d "$PDI_JSON_PATH/sparkler/raw" ]; then
+	echo "Raw sparkler data provided in $PDI_JSON_PATH/sparkler/raw....parsing."
+	pushd $PDI_HOME/insight-generator/;
+	python parse.py $PDI_JSON_PATH/sparkler/raw/sparkler_rawdata.json $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1 ;
+	popd
+	INGEST=1
+    elif [ -d "$PDI_JSON_PATH/sparkler/parsed" ]; then
+	echo "Parsed sparkler data provided in $PDI_JSON_PATH/sparkler/parsed...preparing to ingest."
+	cp $PDI_JSON_PATH/sparkler/parsed/sparkler_data.json $PDI_JSON_PATH/sparkler/ingest/ingest_data.json;
+	INGEST=1
+    elif [ -d "$PDI_JSON_PATH/files" ]; then
+	echo "Calling Apache Tika and prepping JSONs on $PDI_JSON_PATH/files: outputting to $PDI_JSON_PATH/ingest"
+	pushd $PDI_HOME/insight-generator/;
+	python tika_prepare.py $PDI_JSON_PATH/files $PDI_JSON_PATH/ingest >> /deploy/requirements/logs/pdi-ingest.log 2>&1 ;
+	popd
+	INGEST=1
+    fi
 
+    if [[ ! -z "${INGEST}" ]]; then
+	echo "Using ES URL $ES_URL for ingestion."
+	pushd $PDI_HOME/insight-generator/;
+	./elastic_index $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&;
+	popd
+    fi
+	
 fi
 
+
 # watch log
-echo "Watching pdi-generator log...."
-tail -f /deploy/requirements/pdi-generator.log
+echo "Watching pdi-generator log and pdi-ingest log...."
+tail -f /deploy/requirements/logs/pdi-generator.log /deploy/requirements/logs/pdi-ingest.log 

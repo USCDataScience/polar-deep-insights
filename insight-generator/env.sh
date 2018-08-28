b6349983f70829b1b2fc501336108002ce8c4fe3
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2018-08-28 08:44:19 -0700

    cleanup.

--- insight-generator/env.sh
+++ insight-generator/env.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/env bash
-
-# encoding: utf-8
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -20,6 +17,5 @@ export REDIS_ENDPOINT=localhost
 export TIKA_PATH=http://localhost:9998
 export GROBID_QUANTITY_ENDPOINT=http://localhost:8060
 export GEO_TOPIC_ENDPOINT=http://localhost:8765
-export CLASSPATH=/Users/prerana/Desktop/polar-deep-insights/insight-generator/requirements/stanford-ner-2018-02-27/stanford-ner.jar
-export STANDFORD_NER_MODEL_PATH=/Users/prerana/Desktop/polar-deep-insights/insight-generator/requirements/stanford-ner-2018-02-27/classifiers/english.muc.7class.distsim.crf.ser.gz
-echo "Done!!"
+export CLASSPATH=/deploy/requirements/stanford-ner-2018-02-27/stanford-ner.jar
+export STANDFORD_NER_MODEL_PATH=/deploy/requirements/stanford-ner-2018-02-27/classifiers/english.muc.7class.distsim.crf.ser.gz

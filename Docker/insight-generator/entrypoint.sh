acca2cbd824ad3cee579daf6f398b1a442f4a3b1
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2018-09-03 12:12:48 -0700

    Add script for re-ingest; call script from entrypoint. add date to grobid start check.

--- Docker/insight-generator/entrypoint.sh
+++ Docker/insight-generator/entrypoint.sh
@@ -57,42 +57,13 @@ echo "Waiting for services to start"
 while ! grep "Started Jetty Server" /deploy/requirements/logs/pdi-generator.log  > /dev/null;
 do
     sleep 2
-    echo "Waiting for Grobid to start..."
+    echo "$(date) Waiting for Grobid to start..."
 done
 
-if [[ ! -z "${PDI_JSON_PATH}" ]]; then
-
-    if [ -f "$PDI_JSON_PATH/sparkler/raw/sparkler_rawdata.json" ]; then
-	echo "Raw sparkler data provided in $PDI_JSON_PATH/sparkler/raw....parsing."
-	echo "Removing prior ingest data from $PDI_JSON_PATH/ingest/ingest_data.json"
-	rm -rf $PDI_JSON_PATH/ingest/ingest_data.json;
-	pushd $PDI_HOME/insight-generator/;
-	python parse.py $PDI_JSON_PATH/sparkler/raw/sparkler_rawdata.json $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1 ;
-	popd
-	INGEST=1
-    elif [ -f "$PDI_JSON_PATH/sparkler/parsed/sparkler_data.json" ]; then
-	echo "Parsed sparkler data provided in $PDI_JSON_PATH/sparkler/parsed...preparing to ingest."
-	cp $PDI_JSON_PATH/sparkler/parsed/sparkler_data.json $PDI_JSON_PATH/ingest/ingest_data.json;
-	INGEST=1
-    elif [ -d "$PDI_JSON_PATH/files" ]; then
-	echo "Removing prior ingest data from $PDI_JSON_PATH/ingest/ingest_data.json"
-	rm -rf $PDI_JSON_PATH/ingest/ingest_data.json;
-	echo "Calling Apache Tika and prepping JSONs on $PDI_JSON_PATH/files: outputting to $PDI_JSON_PATH/ingest"
-	pushd $PDI_HOME/insight-generator/;
-	python tika-prepare.py $PDI_JSON_PATH/files $PDI_JSON_PATH/ingest >> /deploy/requirements/logs/pdi-ingest.log 2>&1 ;
-	popd
-	INGEST=1
-    fi
-
-    if [[ ! -z "${INGEST}" ]]; then
-	echo "Using ES URL $ES_URL for ingestion."
-	pushd $PDI_HOME/insight-generator/;
-	./elastic_index.sh $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&
-	popd
-    fi
-
-fi
-
+# run the pipeline for ingest
+pushd $PDI_HOME/insight-generator;
+./ingest_pipeline.sh "$PDI_JSON_PATH";
+popd
 
 # watch log
 echo "Watching pdi-generator log and pdi-ingest log...."
---
+++ insight-generator/ingest-pipeline.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PDI_JSON_PATH="$1"
+
+if [[ ! -z "${PDI_JSON_PATH}" ]]; then
+
+    if [ -f "$PDI_JSON_PATH/sparkler/raw/sparkler_rawdata.json" ]; then
+	echo "Raw sparkler data provided in $PDI_JSON_PATH/sparkler/raw....parsing."
+	echo "Removing prior ingest data from $PDI_JSON_PATH/ingest/ingest_data.json"
+	rm -rf $PDI_JSON_PATH/ingest/ingest_data.json;
+	pushd $PDI_HOME/insight-generator/;
+	python parse.py $PDI_JSON_PATH/sparkler/raw/sparkler_rawdata.json $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1 ;
+	popd
+	INGEST=1
+    elif [ -f "$PDI_JSON_PATH/sparkler/parsed/sparkler_data.json" ]; then
+	echo "Parsed sparkler data provided in $PDI_JSON_PATH/sparkler/parsed...preparing to ingest."
+	cp $PDI_JSON_PATH/sparkler/parsed/sparkler_data.json $PDI_JSON_PATH/ingest/ingest_data.json;
+	INGEST=1
+    elif [ -d "$PDI_JSON_PATH/files" ]; then
+	echo "Removing prior ingest data from $PDI_JSON_PATH/ingest/ingest_data.json"
+	rm -rf $PDI_JSON_PATH/ingest/ingest_data.json;
+	echo "Calling Apache Tika and prepping JSONs on $PDI_JSON_PATH/files: outputting to $PDI_JSON_PATH/ingest"
+	pushd $PDI_HOME/insight-generator/;
+	python tika-prepare.py $PDI_JSON_PATH/files $PDI_JSON_PATH/ingest >> /deploy/requirements/logs/pdi-ingest.log 2>&1 ;
+	popd
+	INGEST=1
+    fi
+
+    if [[ ! -z "${INGEST}" ]]; then
+	echo "Using ES URL $ES_URL for ingestion."
+	pushd $PDI_HOME/insight-generator/;
+	./elastic_index.sh $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&
+	popd
+    fi
+
+fi

a0ab20ee1a5a07e9eacb9c8d9eaa5816b3a2f1e1
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2018-09-04 11:44:42 -0700

    measurement-index.sh not measurement-ingest.

--- insight-generator/ingest-pipeline.sh
+++ insight-generator/ingest-pipeline.sh
@@ -45,7 +45,7 @@ if [[ ! -z "${PDI_JSON_PATH}" ]]; then
 	pushd $PDI_HOME/insight-generator/;
 	./elastic_index.sh $PDI_JSON_PATH/ingest/ingest_data.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&
 	python ./measurement-index.py ./out/ ./measurement-index.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&
-	./measurement-ingest.sh ./measurement-index.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&
+	./measurement-index.sh ./measurement-index.json >> /deploy/requirements/logs/pdi-ingest.log 2>&1&
 	popd
     fi
 
